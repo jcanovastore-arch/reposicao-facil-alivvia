07d8ee8d56664cf4c0a92f93a8a87efb185b9684
jcanovastore-arch <jcanovastore@gmail.com>
2025-11-21 12:08:10 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+from typing import Any
+from fastapi import FastAPI, Body
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(title="API Reposição Alivvia v4 (debug)")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/health")
+def health():
+    print(">> /health foi chamado")
+    return {"status": "ok"}
+
+@app.post("/calcular-compra")
+async def api_calcular_compra(body: dict = Body(...)) -> Any:
+    print("\n===============================")
+    print("PAYLOAD RECEBIDO EM /calcular-compra:")
+    print(body)
+    print("===============================\n")
+
+    return body
